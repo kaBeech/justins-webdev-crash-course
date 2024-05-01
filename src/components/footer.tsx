604bd99b934abef184215c7a42f71ddc07f3afef
kaBeech <contact@kaBeech.com>
2024-04-30 21:23:05 -0700

    Handle very small screens

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='marginBottom1 marginLeft2 marginRight2 flex column alignCenter gapPoint5'>
-            <  div className="flex gap2 textBigger">
+            <  div className="textBigger flex gap2 wrap">
                 {/* These icons are made using a Nerd Font */}
                 <a href="https://www.linkedin.com/in/justinjamesshort/"></a>
                 <a href="http://soundcloud.com/holdyourcomposer"></a>
