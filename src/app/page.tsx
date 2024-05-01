65658963edc5ad6f3ce5a36f2f1e18bb5795104c
kaBeech <contact@kaBeech.com>
2024-04-30 20:56:15 -0700

    Standardize punctuation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>
-        Welcome to Justins Music Site!!!
+        Welcome to Justin's Music Site!
       </h1>
       <p className="marginTop4 marginBottomAuto textGiant"></p>
     </main>
