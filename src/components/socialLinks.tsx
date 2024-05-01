042558811f32a1064322e475771b26e0b33bf08a
kaBeech <contact@kaBeech.com>
2024-05-01 14:47:41 -0700

    Fix typo

--- src/components/socialLinks.tsx
+++ src/components/socialLinks.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SocialLinks = () => {
     return (
-        <  div className="textBigger flex gap2 wrap">
+        <div className="textBigger flex gap2 wrap">
             {/* These icons are made using a Nerd Font */}
             <a href="https://www.linkedin.com/in/justinjamesshort/"></a>
             <a href="http://soundcloud.com/holdyourcomposer"></a>
