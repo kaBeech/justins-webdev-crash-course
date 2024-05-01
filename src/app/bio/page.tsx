b4688c50f38bdb65099b7a08cce3f16ca2f55264
kaBeech <contact@kaBeech.com>
2024-04-30 19:54:44 -0700

    Add boilerplate routes

---
+++ src/app/bio/page.tsx
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+export default function Bio() {
+  return (
+    <main className={styles.main}>
+      <h1>
+        Bio
+      </h1>
+    </main>
+  );
+}
---
+++ src/app/music/page.tsx
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+export default function Music() {
+  return (
+    <main className={styles.main}>
+      <h1>
+        Music
+      </h1>
+    </main>
+  );
+}
