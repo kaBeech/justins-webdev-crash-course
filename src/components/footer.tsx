acdddff7e0372c26132ea1a0d558ba4ed4702528
kaBeech <contact@kaBeech.com>
2024-04-30 19:54:59 -0700

    Add explanatory comment

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <footer className='flex column alignCenter gapPoint5'>
             <  div className="flex gap2 textBigger">
+                {/* These icons are made using a Nerd Font */}
                 <a href="https://www.linkedin.com/in/justinjamesshort/"></a>
                 <a href="http://soundcloud.com/holdyourcomposer"></a>
                 <a href="https://www.instagram.com/holdyourcomposer/"></a>
