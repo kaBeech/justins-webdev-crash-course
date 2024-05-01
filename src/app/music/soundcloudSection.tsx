cfa7dba43d97815f7623d1262f18974fdb4e7cbf
kaBeech <contact@kaBeech.com>
2024-05-01 16:13:20 -0700

    Adjust spacing on the page

--- src/app/music/soundcloudSection.tsx
+++ src/app/music/soundcloudSection.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 const SoundCloudSection = () => {
   return (
     <div>
-      <iframe width="100%" height="300" scrolling="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1616082534&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-      <div >
-        <a href="https://soundcloud.com/msmsmsm" title="SOPHIE" target="_blank" >SOPHIE</a> · <a href="https://soundcloud.com/msmsmsm/hard" title="HARD" target="_blank" >HARD</a></div>
+      <iframe className="marginTop4" width="100%" height="300" scrolling="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1616082534&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+      <a className="marginBottom4" href="https://soundcloud.com/msmsmsm" title="SOPHIE" target="_blank" >SOPHIE</a> · <a href="https://soundcloud.com/msmsmsm/hard" title="HARD" target="_blank" >HARD</a>
     </div>
   );
 }
