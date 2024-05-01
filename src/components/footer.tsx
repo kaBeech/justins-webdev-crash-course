6d39bbef9eb367b49c67fe4c9f96719726e99f2a
kaBeech <contact@kaBeech.com>
2024-04-30 21:22:45 -0700

    Make sure there's some room on the sides of the screen

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='marginBottom1 flex column alignCenter gapPoint5'>
+        <footer className='marginBottom1 marginLeft2 marginRight2 flex column alignCenter gapPoint5'>
             <  div className="flex gap2 textBigger">
                 {/* These icons are made using a Nerd Font */}
                 <a href="https://www.linkedin.com/in/justinjamesshort/"></a>
